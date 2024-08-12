bcf924efb7192d20970b8f601f0d6713263a8124
TomH35 <132070682+TomH35@users.noreply.github.com>
2024-08-12 14:18:46 +0200

    Created AdminRegistrationAPI.php

---
+++ Backend/api/AdminRegistrationAPI.php
@@ -0,0 +1,14 @@
+<?php
+// Include the Composer autoloader
+require '../vendor/autoload.php';
+
+// Include the Database class
+require '../Database.php';
+
+function AdminRegistration(){
+
+}
+
+
+
+?>
