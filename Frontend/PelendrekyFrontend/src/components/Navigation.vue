cb0568f421ec8f1e382ae963b01beb19afc45d7e
TomH35 <132070682+TomH35@users.noreply.github.com>
2024-08-13 15:03:04 +0200

    minor changes in the frontend

--- Frontend/PelendrekyFrontend/src/components/Navigation.vue
+++ Frontend/PelendrekyFrontend/src/components/Navigation.vue
@@ -3,7 +3,7 @@
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
           <img src="" alt="logo" width="28" height="28" class="d-inline-block align-text-top">
-          <span class="custom-navbar-top-color">Včelie úle</span>
+          <span class="custom-navbar-top-color"></span>
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
--- Frontend/PelendrekyFrontend/src/views/HomeView.vue
+++ Frontend/PelendrekyFrontend/src/views/HomeView.vue
@@ -1,37 +1,9 @@
 <template>
   <main>
-    <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
-      <div class="card w-50">
-        <div class="card-body">
-          <h5 class="card-title text-center">Prihlásiť sa</h5>
-          <LoginForm @login="loginAsGuest" />
-        </div>
-      </div>
-    </div>
+ 
   </main>
 </template>
 
 <script>
-import { useLoginStore } from '../stores/loginStore';
-import LoginForm from '../components/LoginForm.vue';
-import { useRouter } from 'vue-router';
 
-export default {
-  components: {
-    LoginForm,
-  },
-  setup() {
-    const store = useLoginStore();
-    const router = useRouter();
-
-    const loginAsGuest = async () => {
-      await store.loginAsGuest();
-      router.push('/BeehiveSelection');
-    };
-
-    return {
-      loginAsGuest,
-    };
-  },
-};
 </script>
