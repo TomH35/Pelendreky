b44798eb910726e125652feba46131d451957c23
TomH35 <132070682+TomH35@users.noreply.github.com>
2024-08-14 12:11:24 +0200

    Added admin panel

--- Frontend/PelendrekyFrontend/src/views/AdminMainMenuView.vue
+++ Frontend/PelendrekyFrontend/src/views/AdminMainMenuView.vue
@@ -1,9 +1,85 @@
 <template>
-    <main>
-   
-    </main>
+  <div class= "container mt-5 mb-5 d-flex justify-content-center">
+  <div class= "row mt-5 mb-5">
+  <div class= "col-sm-6 mt-5 mb-5">
+  <div class="card mb-5 mt-5" style="max-width: 540px;">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="/img/add-g273330623_1280.png" class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">Pridať alebo upraviť článok</h5>
+          <p class="card-text"></p>
+          
+              <span class="btn custom-button-color custom-button-margin">Pridavanie</span>
+         
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  <div class="col-sm-6 mt-5 mb-5">
+    <div class="card mb-5 mt-5" style="max-width: 540px;">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="/img/trash-g0d0eff27b_1280.png" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Zmazať článok</h5>
+            <p class="card-text"></p>
+            
+              <span class="btn custom-button-color custom-button-margin">Mazanie</span>
+          
+          </div>
+        </div>
+      </div>
+    </div>
+  
+  
+  </div>
+  </div>
+  </div>
+  <div class="container d-flex justify-content-center mt-3 mb-5">
+      <button @click="handleLogout" class="btn custom-button-color">Logout</button>
+  </div>
+  
   </template>
-  
   <script>
+  import { useLoginStore } from '../stores/loginStore';
+  import { useRouter } from 'vue-router';
   
+  export default {
+    name: 'adminPanel',
+    data() {
+      return {
+        user: useLoginStore(),
+        router: useRouter()
+      }
+    },
+    methods: {
+        async handleLogout() {
+            try {
+                const response = await fetch('./Backend/admin_api/AdminLogoutAPI.php', {
+                  method: 'POST',
+                  headers: {
+                    'Authorization': `Bearer ${this.user.getToken()}`, // Include the JWT token in the header
+                    'Content-Type': 'application/json'
+                  }
+            });
+
+              if (!response.ok) {
+                throw new Error('Failed to logout');
+            } 
+
+            this.user.clearToken(); // Clear the token from the store and local storage
+            this.router.push({ name: 'adminLogin' }); // Redirect to the login page
+
+          } catch (error) {
+            console.error('Logout error:', error);
+          }
+      }
+    }
+  }
   </script>
