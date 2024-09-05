9035c6745e972dbc8e6b3498324a2f6497b1cbb0
TomH35 <132070682+TomH35@users.noreply.github.com>
2024-09-05 14:09:13 +0200

    Updated navbar to dynamically display categories

--- Frontend/PelendrekyFrontend/src/components/Navigation.vue
+++ Frontend/PelendrekyFrontend/src/components/Navigation.vue
@@ -1,45 +1,61 @@
 <template>
-    <nav class="navbar navbar-expand-sm navbar-dark fixed-top custom-background">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          <img src="" alt="logo" width="28" height="28" class="d-inline-block align-text-top">
-          <span class="custom-navbar-top-color"></span>
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item" v-for="(url, page) in headerMenu" :key="page">
-              <router-link :to="url" class="nav-link" @click="logoutIfNecessary(page)">
+  <nav class="navbar navbar-expand-sm navbar-dark fixed-top custom-background">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="" alt="logo" width="28" height="28" class="d-inline-block align-text-top">
+        <span class="custom-navbar-top-color"></span>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item" v-for="(url, page) in headerMenu" :key="page">
+            <router-link :to="url" class="nav-link" @click="logoutIfNecessary(page)">
               <span class="custom-navbar-color">{{ page }}</span>
-              </router-link>
-            </li>
-          </ul>
-        </div>
+            </router-link>
+          </li>
+          <!-- Dynamic category links -->
+          <li v-for="category in categories" :key="category.category_id" class="nav-item">
+            <router-link :to="`/${category.category_slug}`" class="nav-link">
+              <span class="custom-navbar-color">{{ category.category_name }}</span>
+            </router-link>
+          </li>
+        </ul>
       </div>
-    </nav>
-  </template>
-  
-  <script>
-  import { useLoginStore } from '../stores/loginStore';
-  
-  export default {
-    data() {
-      return {
-        headerMenu: {
-          "Logout": "/",
-          // ...other menu items...
-        },
-      };
-    },
-    methods: {
-      logoutIfNecessary() {
-        if (page === 'Logout') {
-          const loginStore = useLoginStore();
-          loginStore.logout();
-        }
+    </div>
+  </nav>
+</template>
+
+<script>
+import { useLoginStore } from '../stores/loginStore';
+import { useCategoryStore } from '../stores/CategoryStore'; // Import your category store
+
+export default {
+  data() {
+    return {
+      headerMenu: {
+        "Logout": "/",
+        // Add any other static menu items here...
       },
+      categories: [], // To store the fetched categories
+    };
+  },
+  methods: {
+    logoutIfNecessary(page) {
+      if (page === 'Logout') {
+        const loginStore = useLoginStore();
+        loginStore.logout();
+      }
     },
-  };
-  </script>
+    async fetchCategories() {
+      const categoryStore = useCategoryStore();
+      await categoryStore.fetchCategories(); // Fetch categories from the API
+      this.categories = categoryStore.getCategories; // Store them locally
+    },
+  },
+  mounted() {
+    this.fetchCategories(); // Fetch categories when the component is mounted
+  },
+};
+</script>
