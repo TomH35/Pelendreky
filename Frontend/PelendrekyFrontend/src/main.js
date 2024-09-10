7b2cb8d804e719fadd597e53de4bad99cc083859
TomH35 <132070682+TomH35@users.noreply.github.com>
2024-09-10 15:55:32 +0200

    Added Quill WYSIWYG editor

--- Frontend/PelendrekyFrontend/src/main.js
+++ Frontend/PelendrekyFrontend/src/main.js
@@ -2,6 +2,7 @@ import './assets/style.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
+import 'quill/dist/quill.snow.css';
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { useLoginStore } from './stores/loginStore';
